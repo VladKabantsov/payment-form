876683bdbc8ee6c19faa5d05262b3920c358773c
Vlad Kabantsov <vkabantsov@aimc.io>
2019-03-13 00:46:27 +0300

    add template

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MainInfoComponent } from './components/main-info/main-info.component';
+import { CardFormComponent } from './components/card-form/card-form.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { LanguagesComponent } from './components/languages/languages.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { PaymentProcessedComponent } from './components/payment-processed/payment-processed.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainInfoComponent,
+    CardFormComponent,
+    UserFormComponent,
+    LanguagesComponent,
+    PaymentProcessedComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/payment-processed/payment-processed.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaymentProcessedComponent } from './payment-processed.component';
+
+describe('PaymentProcessedComponent', () => {
+  let component: PaymentProcessedComponent;
+  let fixture: ComponentFixture<PaymentProcessedComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PaymentProcessedComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PaymentProcessedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
